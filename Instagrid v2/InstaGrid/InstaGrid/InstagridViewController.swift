7e047d19ae71e140270c95ec83f200a8b533df9f
Bananathoms <116547351+Bananathoms@users.noreply.github.com>
2023-09-23 02:47:27 +0200

    Ajout des UITapGesture

--- Instagrid v2/InstaGrid/InstaGrid/InstagridViewController.swift
+++ Instagrid v2/InstaGrid/InstaGrid/InstagridViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class InstagridViewController: UIViewController {
+    
 
+    @IBOutlet weak var gridView: GridView!
+    
+    @IBOutlet weak var layout1Button: UIImageView!
+    
+    @IBOutlet weak var layout2Button: UIImageView!
+    
+    @IBOutlet weak var layout3Button: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func layout1ImageTapped(_ sender: UITapGestureRecognizer) {
+        gridView.layoutType = .layout1
+    }
+
+    @IBAction func layout2ImageTapped(_ sender: UITapGestureRecognizer) {
+        gridView.layoutType = .layout2
+    }
+
+    @IBAction func layout3ImageTapped(_ sender: UITapGestureRecognizer) {
+        gridView.layoutType = .layout3
+    }
 
 }
 
