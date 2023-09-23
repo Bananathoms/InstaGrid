3d20057f27f44e02ec6b786b14cc866e8abee180
Bananathoms <116547351+Bananathoms@users.noreply.github.com>
2023-09-23 03:08:35 +0200

    ajout de l'image selectedLayout lors de la selection du layout

--- Instagrid v2/InstaGrid/InstaGrid/InstagridViewController.swift
+++ Instagrid v2/InstaGrid/InstaGrid/InstagridViewController.swift
@@ -9,13 +9,14 @@ import UIKit
 
 class InstagridViewController: UIViewController {
     
+    let selectedLayout = UIImage(named: "Selected")
+    let layout1Image = UIImage(named: "Layout 1")
+    let layout2Image = UIImage(named: "Layout 2")
+    let layout3Image = UIImage(named: "Layout 3")
 
     @IBOutlet weak var gridView: GridView!
-    
     @IBOutlet weak var layout1Button: UIImageView!
-    
     @IBOutlet weak var layout2Button: UIImageView!
-    
     @IBOutlet weak var layout3Button: UIImageView!
     
     override func viewDidLoad() {
@@ -25,14 +26,23 @@ class InstagridViewController: UIViewController {
 
     @IBAction func layout1ImageTapped(_ sender: UITapGestureRecognizer) {
         gridView.layoutType = .layout1
+        layout1Button.image = selectedLayout
+        layout2Button.image = layout2Image
+        layout3Button.image = layout3Image
     }
 
     @IBAction func layout2ImageTapped(_ sender: UITapGestureRecognizer) {
         gridView.layoutType = .layout2
+        layout1Button.image = layout1Image
+        layout2Button.image = selectedLayout
+        layout3Button.image = layout3Image
     }
 
     @IBAction func layout3ImageTapped(_ sender: UITapGestureRecognizer) {
         gridView.layoutType = .layout3
+        layout1Button.image = layout1Image
+        layout2Button.image = layout2Image
+        layout3Button.image = selectedLayout
     }
 
 }
